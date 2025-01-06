ea8d69d8786c7eda3228d18ee9fab0e870df6245
Thierno Diallo <tdiallo1996@gmail.com>
2025-01-05 21:05:45 -0500

    Commit Code

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Button("Tap This") {
+                withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                    animationAmount += 360
+                }
+                
+            }
+            .padding(50)
+            .background(.purple)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
         }
-        .padding()
     }
 }
 
