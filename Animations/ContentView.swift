dca1ac66b55351935b9918a6fd2500d18394bc0d
Thierno Diallo <tdiallo1996@gmail.com>
2025-01-06 21:59:33 -0500

    Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,24 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
-    var body: some View {
-        
+    @State private var isShowing = false
+        var body: some View {
         VStack {
-            
             Button("Tap This") {
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    animationAmount += 360
+                withAnimation {
+                    isShowing.toggle()
                 }
-                
             }
-            .padding(50)
-            .background(.purple)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+            
+            if isShowing {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.opacity)
+            }
         }
     }
+    
 }
 
 #Preview {
